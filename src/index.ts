a076323853a003af59f4d6b1a0b0eda6db59b74e
Rhynel <rhynel.dev@gmail.com>
2024-12-20 03:53:03 +0800

    Update Doc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from './formatters/number'
 export * from './formatters/string';
 export * from './formatters/date'
+export * from './formatters/array'
+export * from './formatters/object'
 export { _, formatters } from './chain';
